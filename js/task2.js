fbff7785cad5f2d2fd3d2655aba4d56ef53f40e4
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-11-11 19:36:31 +0200

    task2 done

--- js/task2.js
+++ js/task2.js
@@ -13,16 +13,24 @@
 
 
 
-const calculateEngravingPrice = (message = "", pricePerWord = 0) => pricePerWord; // Write code in this line
+const calculateEngravingPrice = function (message = "", pricePerWord = 0) {
+    let messageSplit = message.split(' ')
+    // console.log(messageSplit)
+    let messageLength = messageSplit.length;
+    // console.log(messageLength);
+    let result = messageLength * pricePerWord;
+    
+    return result
+}; // Write code in this line
 
-// console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 10)); // 80
+console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 10)); // 80
 
-// console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 20)); // 160
+console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 20)); // 160
 
-// console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40)); // 240
+console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40)); // 240
 
-// console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20)); // 120
+console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20)); // 120
 
-// console.log(calculateEngravingPrice('Uno', 100)); // 100
+console.log(calculateEngravingPrice('Uno', 100)); // 100
 
 
