3cf7495c0874f2145c32a612ab659106313a7563
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-11-11 22:06:41 +0200

    work on task4

---
+++ js/task4.js
@@ -0,0 +1,23 @@
+'use strict'
+
+function formatString (string, maxLength = 40) {
+    let stringLength = string.length;
+    // console.log(stringLength);;
+    if (stringLength <= maxLength) {
+        console.log(string);
+    } else 
+    return
+  }
+  
+  console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+  // 'Curabitur ligula sapien, tincidunt non.'
+  
+  console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+  // 'Vestibulum facilisis, purus nec pulvinar...'
+  
+  console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.', 30));
+  // 'Vestibulum facilisis, purus ne...'
+
+//   const string = 'Curabitur ligula sapien, tincidunt non.'
+//   let stringLength = string.length;
+//   console.log(stringLength);
