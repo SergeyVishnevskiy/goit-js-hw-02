c81cb36c30cf2a1ffce6c1efc443846d629cb63e
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-11-11 16:41:20 +0200

    practice

---
+++ js/practice.js
@@ -0,0 +1,21 @@
+// const favorite = 'the road will be mastered be the walking';
+// const items = 'the roadasdsdasdas will be mastered be the walking';
+// // console.log(favorite.length);
+// const findLongestWord = function (string) {
+//     const words = string.split(' ');
+//     let longestWord = words[0];
+//     let length = words.length;
+//     for (let i=0; i<length; i+=1) {
+//         // console.log(words[i]);
+//     if (longestWord.length < words[i].length) {
+//         longestWord = words[i];
+//         }
+
+//     }
+//     return longestWord;
+// }
+// console.log(findLongestWord(favorite));
+// console.log(findLongestWord(items));
+
+// Aa Bb Cc Dd Ee Ff Gg Hh Ii Jj Kk Ll Mm Nn Oo Pp Qq Rr Ss Tt Uu Vv Ww Xx Yy Zz
+// a b c d e f g h i j k l m n o p q r s t u v w x y z
