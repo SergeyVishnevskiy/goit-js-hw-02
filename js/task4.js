646cc48edd1e25443b7917cc47740e2559e69c80
SergeyVishnevskiy <70287472+SergeyVishnevskiy@users.noreply.github.com>
2020-11-12 16:09:36 +0200

    task4 done

--- js/task4.js
+++ js/task4.js
@@ -2,8 +2,13 @@
 
 function formatString (string, maxLength = 40) {
     
-  
+    if (string.length < maxLength) {
+      return string
+    } else {return string.slice(0, maxLength) + '...';
+    }
   }
+
+ 
   
   console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
   // 'Curabitur ligula sapien, tincidunt non.'
